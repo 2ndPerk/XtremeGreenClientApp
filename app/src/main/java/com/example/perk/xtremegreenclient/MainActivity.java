cdd1f04d22e9db60aa7d8e1113fd7e566e0e1213
2ndPerk <tymonek.wl@gmail.com>
2017-06-10 15:52:04 -0300

    test for submoduling

--- app/src/main/java/com/example/perk/xtremegreenclient/MainActivity.java
+++ app/src/main/java/com/example/perk/xtremegreenclient/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.perk.xtremegreenclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//comment bloblobobobl
+//comment
 
 public class MainActivity extends AppCompatActivity {
 
