dbc1648ce4706e18cfe6b1734850a1146fc758a2
2ndPerk <tymonek.wl@gmail.com>
2017-06-10 15:14:41 -0300

    bla

--- app/src/main/java/com/example/perk/xtremegreenclient/MainActivity.java
+++ app/src/main/java/com/example/perk/xtremegreenclient/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.perk.xtremegreenclient;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//comment
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
