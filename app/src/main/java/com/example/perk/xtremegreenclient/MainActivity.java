724a09ea6115ab87a336d59b08a416326fca5068
micha <michael_fisher_15@hotmail.com>
2017-07-06 11:31:58 -0300

    Added method which can retrieve the humidity value from Firebase and display in on the Humidity TextView on the UI.
    -Michael and Will

--- app/src/main/java/com/example/perk/xtremegreenclient/MainActivity.java
+++ app/src/main/java/com/example/perk/xtremegreenclient/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.perk.xtremegreenclient;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 //comment
 
@@ -23,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private double hum2 = 18.7;
     private double hum3 = 15.2;
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference ref = database.getReference();
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference XtremeGreenHR, XtremeGreenHum, XtremeGreenLight, XtremeGreenTemp;
     //Firebase.setAndroidContext(this);
 
 
@@ -32,14 +36,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //XtremeGreen = FirebaseDatabase.getInstance().getReferenceFromUrl("https://xtremegreen-group9.firebaseio.com/");
+        XtremeGreenHum = FirebaseDatabase.getInstance().getReference("Humidty");
+        //XtremeGreen = XtremeGreen.child("xtremegreen-group9");
+
         tv1 = (TextView) findViewById(R.id.tempText);
         tv2 = (TextView) findViewById(R.id.lightText);
         tv3 = (TextView) findViewById(R.id.humidityText);
 
-    }
+        //final Query humidityQuery = XtremeGreen.orderByChild("Humidity");
 
-}
+        tv3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                XtremeGreenHum.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String obj = (String) dataSnapshot.getValue();
+                        tv3.setText(obj);
+                    }
+                    public void onCancelled(DatabaseError databaseError) {
+                        System.out.println("Error.");
+                    }
+            });
 
+
+
+        };
+        /*XtremeGreen.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                dataSnapshot.getChildren().iterator().next().getValue(GreenhouseVariables.class);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });*/
+
+        });
+
+    }}
 
 
 
